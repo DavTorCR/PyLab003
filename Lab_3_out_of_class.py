88a861f53f99f696172b6e8e76843144ec4328bf
David Toribio <hollow.prolific.vegabond@gmail.com>
2026-02-07 23:56:27 -0800

    the start - 02-07-26 - 9:07 pm the end - 11:56 pm

--- Lab_3_out_of_class.py
+++ Lab_3_out_of_class.py
@@ -1,1 +1,69 @@
 print("Hello World.")
+
+#this lab we will build a basic peer to peer chat system using existing python
+# libraries and the lab_chat.py file shared in this repo. The goal is to create functions
+# that allows users to join a chat group and send and receive messages.
+#we'll explore core concepts of pyton defining function, parameters,
+# simple loops, and importing and calling existing functions.
+
+#This lab is divided into four parts and should take approx. two hours to complete.
+#"I'm hoping I can manage that as opposed to taking longer like I usually do."#
+
+#Pre Reqs: install zero mq pyre.
+
+#Part one: Core functions, Data handling and user input.
+#we'll create several basic functions to handle user input and data that will be used in the later stages
+#we will begin by practicing creating proper functions headers and bodies.
+
+#This is a line break function, it displays a line break by printing out one hundred dashes
+def lb():
+    print("-"*100)
+#zeromq0pyre installed using the pycharm python packages left hand menu, The Python Packages I
+# also have installed shows that ipaddress(1.0.23), pip(25.0.1), pyzmq(27.1.0), zeromq-pyre(0.3.4)
+
+lb()
+
+import lab_chat as lc
+
+
+def get_user_input(messages, to_upper = True):
+    if to_upper:
+        response = input(messages).strip().upper()
+    else:
+        response = input(messages).strip()
+    return response
+
+def get_username():
+      return get_user_input("Please enter your preferred User Name: ")
+
+def get_group():
+    return get_user_input("Please enter the Name of the Group you would like to join: ")
+
+def get_message():
+    return get_user_input("Please enter the message you would like to send: ", False)
+
+def initialize_chat():
+    users = get_username()
+    groups = get_group()
+    node = lc.get_peer_node(users)
+    lc.join_group(node, groups)
+    channel = lc.get_channel(node, groups)
+    return channel
+
+def start_chat():
+    channel = initialize_chat()
+
+    while True:
+        try:
+            msg = get_message()
+            channel.send(msg.encode('utf-8'))
+        except (KeyboardInterrupt, SystemExit):
+            break
+        channel.send("$$$STOP".encode('utf-8'))
+        print("FINISHED")
+
+printstart_chat()
+
+
+
+
---
+++ readme3.md
@@ -0,0 +1,54 @@
+# Identifying Functions
+
+## For each function in the lab chat file Identify the Funciton, Parameters, and what line does it start and end on  to your README.md file
+
+### The First;
+The Function is: Defined get underscore peer underscore node
+
+The Parameters are: username
+
+It begins on Line: 9
+
+It ends on Line: 14
+
+_Return is used, it returns n_
+
+
+### The Second;
+The Function is: Defined join underscore group
+
+The Parameters are: node, group
+
+It begins on Line: 16
+
+It ends on Line: 18
+
+_Return is not used_
+
+### The Third;
+The Function is: Defined chat underscore task
+
+The Parameters are: ctx, pipe, n, group
+
+It begins on Line: 20
+
+It ends on Line: 58
+
+_Return is not used_
+
+___This is the longest function in lab chat file.___
+
+
+### The Fourth;
+The Function is: Defined get underscore channel
+
+The Parameters are: node, group
+
+It begins on Line: 60
+
+It ends on Line: 62
+
+_Return is used, it returns zhelper.zthread_fork(ctx, chat_task, n=node, group=group)_
+
+___This is the shortest function in lab chat file.___
+
